f766e327a164431367388b3f276c6635bc997b9e
EZillman <evelina.zillman@medieinstitutet.se>
2024-01-16 22:11:36 +0100

    [added] style mobile nav

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -3,23 +3,27 @@
 <template>
     <nav>
         <ul>
-            <NuxtLink to="/home">
+            <NuxtLink to="/home" exact-active-class="active-link">
                 <li>
+                    <img src="/home.png" height="49" width="49"> 
                     Home
                 </li>
             </NuxtLink>
-            <NuxtLink to="/members">
+            <NuxtLink to="/members" exact-active-class="active-link">
                 <li>
+                    <img src="/friends.png" height="49" width="49">
                     Members
                 </li>
             </NuxtLink>
-            <NuxtLink to="chores">
+            <NuxtLink to="chores" exact-active-class="active-link">
                 <li>
+                    <img src="/vacuum-cleaner.png" height="49" width="49">
                     Chores
                 </li>
             </NuxtLink>
-            <NuxtLink to="settings">
+            <NuxtLink to="settings" exact-active-class="active-link">
                 <li>
+                    <img src="/setting.png" height="49" width="49">
                     Settings
                 </li>
             </NuxtLink>
@@ -34,4 +38,47 @@
 
 <style lang="scss" scoped>
 
+nav {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    background-color: #324B4B;
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 0;
+        margin: 0.2rem;
+
+        a {
+            background-color: #4D6C6C;
+            margin: 0 0.1rem;
+            padding: 1rem 0.3rem;
+            width: 25%;
+            text-align: center;
+
+            li {
+                text-decoration: none;
+                list-style-type: none;
+                color: #D4F5F4;
+            }    
+            
+            &.active-link {
+                background-color: #324B4B;
+            }
+        }
+
+     
+    }
+
+}
+
+@media screen and (min-width: 760px) {
+
+}
+
+@media screen and (min-width: 1024px) {
+
+}
 </style>
