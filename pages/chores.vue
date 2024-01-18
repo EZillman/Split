d337a41a125cc9cc4ba2f9e987a3bdb7b42af397
EZillman <evelina.zillman@medieinstitutet.se>
2024-01-18 23:12:20 +0100

    [added] open and closing add members and chores

--- pages/chores.vue
+++ pages/chores.vue
@@ -2,13 +2,27 @@
     <HeaderNav></HeaderNav>
     <main>
         <h2>Chores</h2>
-        <CreateChoresForm></CreateChoresForm>
+
+        <div class="add-btn-container">
+            <h3>Add chores</h3>
+            <button @click="toggleAddChores">
+                <img v-if="isAddChoresOpen" src="/up.png" height="30" width="30">
+                <img v-else src="/down.png" height="30" width="30">
+            </button>            
+        </div>
+
+        <CreateChoresForm v-if="isAddChoresOpen"></CreateChoresForm>
         <ChoresList></ChoresList>
         
     </main>
 </template>
 
 <script setup>
+const isAddChoresOpen = ref(false);
+
+const toggleAddChores = () => {
+  isAddChoresOpen.value = !isAddChoresOpen.value;
+};
 
 </script>
 
--- pages/members.vue
+++ pages/members.vue
@@ -3,16 +3,28 @@
     <main>
         <h2>Members</h2>
 
-        <CreateMemberForm></CreateMemberForm>
+        <div class="add-btn-container">
+            <h3>Add members</h3>
+            <button @click="toggleAddMembers">
+                <img v-if="isAddMembersOpen" src="/up.png" height="30" width="30">
+                <img v-else src="/down.png" height="30" width="30">
+            </button>            
+        </div>
+
+        <CreateMemberForm v-if="isAddMembersOpen"></CreateMemberForm>
         <MembersList></MembersList>
 
     </main>
 </template>
 
 <script setup>
+const isAddMembersOpen = ref(false);
 
+const toggleAddMembers = () => {
+  isAddMembersOpen.value = !isAddMembersOpen.value;
+};
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
 
 </style>
