3bca2842717917670c3ddd1c0edc6fec1485e4fd
EZillman <evelina.zillman@medieinstitutet.se>
2024-01-22 17:08:22 +0100

    [added] chosen chore data

--- components/ChosenChore.vue
+++ components/ChosenChore.vue
@@ -3,6 +3,8 @@
         Chosen chore
         <p>{{ $route.params.name }}</p>
         <p>{{ store.choreId }}</p>
+        <p>Time estimated: {{ chore.time_estimated }} minutes</p>
+        <p>Monthly frequency: {{ chore.monthly_frequency }} times a month</p>
     </div>
 </template>
 
@@ -10,9 +12,29 @@
 import { useChoreStore } from '~/store/chore.js';
 
 const store = useChoreStore();
+const supabase = useSupabaseClient();
+const chore = ref({});
 
-//Hämta all chore info
+onMounted(async () => {
+  await fetchChore();
+});
 
+async function fetchChore() {
+  try {
+    const { data, error } = await supabase
+      .from('chores')
+      .select('*')
+      .eq('id', store.choreId)
+      .single(); 
+
+    if (error) throw error;
+    
+    chore.value = data;
+    console.log('data', data);
+  } catch (error) {
+    console.error('Error fetching chore:', error.message);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
