489bc7047c7a6c6d0697f84c3260d86a1005ab58
EZillman <evelina.zillman@medieinstitutet.se>
2024-02-06 00:06:38 +0100

    [added] title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,12 @@ export default defineNuxtConfig({
     '@nuxtjs/supabase',
     '@pinia/nuxt',
   ],
+  app: {
+    head: {  
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Split',
+
+    }
+  }
 })
